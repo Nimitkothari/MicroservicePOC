7a47a6c384618b2a5f71ed3ded2d2896c89ca6db
Nimitkothari <kotharinimit8@gmail.com>
2017-12-11 23:57:04 +0530

    Update app.py

--- app.py
+++ app.py
@@ -11,20 +11,23 @@ print('after model loaded')
 
 @app.route('/predict', methods=['POST'])
 def get_prediction():
-    req_body = request.get_json(force=True)
-    param1 = req_body['Size']
-    param2 = req_body['Bedrooms']
-    param3 = req_body['Age']
-    param4 = req_body['Bathrooms']
-    # Predict
-    pred = linReg.predict([[param1, param2, param3,param4]])
-    result = pred[0]
-    msg = {
+    try:
+        req_body = request.get_json(force=True)
+        param1 = req_body['Size']
+        param2 = req_body['Bedrooms']
+        param3 = req_body['Age']
+        param4 = req_body['Bathrooms']
+        # Predict
+        pred = linReg.predict([[param1, param2, param3,param4]])
+        result = pred[0]
+        msg = {
             "message": "Price is %s" % (result)
         }
-    resp = Response(response=json.dumps(msg),
+        resp = Response(response=json.dumps(msg),
                         status=200,\
                         mimetype="application/json")
-    return resp
+        return resp
+    except Exception as e:
+        print(e)
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=port)
